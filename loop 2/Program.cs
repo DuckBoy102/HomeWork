4f77db7765e8b583cd2d528729622f4974ff4f26
DuckBoy102 <avieln7@gmail.com>
2022-02-06 18:19:40 +0200

    Add project files.

---
+++ loop 2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace loop_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("please enter your child age");
+            int age;
+            int counter1 = 0;
+            int counter2 = 0;
+            for (int i = 0; i < 12 && i >15; i++)
+            {
+                age =int.Parse(Console.ReadLine());
+                if (age > 12 && age < 15)
+                    counter1++;
+            }
+        }
+    }
+}
